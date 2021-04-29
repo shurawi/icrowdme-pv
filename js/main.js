63036f546c81668cf1721355cc386ab9b2cb07e5
shurawi <denis.b.toropov@gmail.com>
2021-04-29 23:17:02 +0700

    fixs bugs

--- js/main.js
+++ js/main.js
@@ -1,26 +1,39 @@
 //toggles state in advantages block's description
-var btnSmp = document.querySelector('.advantages__btn--smp');
-var btnTrs = document.querySelector('.advantages__btn--trs');
-var btnClb = document.querySelector('.advantages__btn--clb');
-var advHead = document.querySelector('.advantages__head');
+let btnSmp = document.querySelector('.advantages__link--smp');
+let btnTrs = document.querySelector('.advantages__link--trs');
+let btnClb = document.querySelector('.advantages__link--clb');
+let advHead = document.querySelector('.advantages__head');
+let headBtnMobile = document.querySelector('.head__btn--mobile');
+let headNavCatalog = document.querySelector('.head__nav--catalog');
 
-btnSmp.addEventListener ("click", function () {
-  btnSmp.classList.add('advantages__btn--active');
-  btnTrs.classList.remove('advantages__btn--active');
-  btnClb.classList.remove('advantages__btn--active');
+btnSmp.addEventListener ("click", function (evt) {
+  evt.preventDefault();
+  btnSmp.classList.add('advantages__link--active');
+  btnTrs.classList.remove('advantages__link--active');
+  btnClb.classList.remove('advantages__link--active');
   advHead.style.color = '#26a6d1';
 })
 
-btnTrs.addEventListener ("click", function () {
-  btnTrs.classList.add('advantages__btn--active');
-  btnClb.classList.remove('advantages__btn--active');
-  btnSmp.classList.remove('advantages__btn--active');
+btnTrs.addEventListener ("click", function (evt) {
+  evt.preventDefault();
+  btnTrs.classList.add('advantages__link--active');
+  btnClb.classList.remove('advantages__link--active');
+  btnSmp.classList.remove('advantages__link--active');
   advHead.style.color = '#add136';
 })
 
-btnClb.addEventListener ("click", function () {
-  btnClb.classList.add('advantages__btn--active');
-  btnSmp.classList.remove('advantages__btn--active');
-  btnTrs.classList.remove('advantages__btn--active');
+btnClb.addEventListener ("click", function (evt) {
+  evt.preventDefault();
+  btnClb.classList.add('advantages__link--active');
+  btnSmp.classList.remove('advantages__link--active');
+  btnTrs.classList.remove('advantages__link--active');
   advHead.style.color = '#959595';
+})
+
+//adds open/close state for head__nav--catalog
+headBtnMobile.addEventListener ('click', function (evt) {
+  evt.preventDefault();
+  headBtnMobile.classList.toggle('head__btn--close');
+  headBtnMobile.classList.toggle('head__btn--open');
+  headNavCatalog.classList.toggle('head__nav--close');
 })
